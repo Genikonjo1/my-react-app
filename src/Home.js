2c22bb7fa480ca8e2df18343909abcc662695796
Genet_Developer <geniamare90@yahoo.com>
2025-04-30 13:58:05 -0400

    my first react work

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import BasicExample from "./BasicExample";
+
+function Home() {
+    return (
+      <div>
+        <h1>Home Page</h1>
+        <p>Welcome to my first React app!</p>
+        <BasicExample/>
+      </div>
+    );
+  }
+  export default Home;
